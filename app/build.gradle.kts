3fab710d20129c2d1789b66c6289ca799a129ee1
janaildesilva22-sys <janaildesilva22@gmail.com>
2026-09-06 19:05:30 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
     namespace = "com.example.tlstunnelmvp"
     compileSdk = 35
@@ -22,17 +23,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-} {
-        applicationId = "com.example.tlstunnelmvp"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
     }
 
     buildFeatures {
